099540c1bfe5338a1e95d88eb157e966c46731e7
John Ribar <john@nagroad.com>
2012-05-10 07:31:54 -0500

    initial commit, rev 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+User.delete_all
+user = User.create! :name => 'admin1', :email => 'admin@darthsubs.com', :password => 'admin1', :password_confirmation => 'admin1'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'cook12', :email => 'cook@darthsubs.com', :password => 'cook12', :password_confirmation => 'cook12'
 puts 'New user created: ' << user2.name
+user3 = User.create! :name => 'joe123', :email => 'joe@blow.com', :password => 'joe123', :password_confirmation => 'joe123'
+puts 'New user created: ' << user3.name
 user.add_role :admin
+user2.add_role :cook
+user3.add_role :user
