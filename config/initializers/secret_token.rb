622fd0af885392f26d09381bc04ad07638b18cf2
John Ribar <john@nagroad.com>
2012-05-09 23:36:06 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darthsubs::Application.config.secret_token = '4eb2ce869e11092d9e034e8b06953b4eecc96468f4ff077f3a13801282d1f51490963f32c1c54851c54ec6195cf95757df623efd679ee3f2968d16f7a9a9ba97'
